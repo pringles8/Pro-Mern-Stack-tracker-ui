15355d4ae73dcb86f8399d5d218ade56b5b520f6
Priyank Shelat <priyank.shelat@uconn.edu>
2020-07-25 01:06:41 -0400

    Troubleshooting

--- server/template.js
+++ server/template.js
@@ -7,7 +7,7 @@ export default function template(body, initialData, userData) {
     <head>
       <meta charset="utf-8">
       <title>Pro MERN Stack</title>
-      <link rel="stylesheet" href="/dist/css/bootstrap.min.css">
+      <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
       <script src="https://apis.google.com/js/api:client.js"></script>
